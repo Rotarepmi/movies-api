5ee7f6381b75f6a7698552240b9117934ad5c5ac
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-04-23 20:09:31 +0200

    Shorten the error message to just message

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -1,5 +1,5 @@
 export function catchAsync(fn) {
     return (req, res, next) => {
-        fn(req, res, next).catch(err => next(err));
+        fn(req, res, next).catch(err => next(err.message));
     }
 }

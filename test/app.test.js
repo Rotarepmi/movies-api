e140432541d9272e9a11ab8456c43db81fd3ba9f
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-04-24 22:10:59 +0200

    Test if app returns response

--- test/app.test.js
+++ test/app.test.js
@@ -2,13 +2,13 @@ let expect = require('chai').expect;
 let axios = require('axios');
 
 it('API should respond for movies req', async () => {
-  const res = await axios.get('http://localhost:8080/movies/');
+    const res = await axios.get('http://localhost:8080/movies/');
 
-  expect(res.status).to.equal(200);
+    expect(res.status).to.equal(200);
 });
 
 it('API should respond for comments req', async () => {
-  const res = await axios.get('http://localhost:8080/comments/');
+    const res = await axios.get('http://localhost:8080/comments/');
 
-  expect(res.status).to.equal(200);
+    expect(res.status).to.equal(200);
 });

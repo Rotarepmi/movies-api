99a26531c1dd946bc0bac2b6f26d725be982888d
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-04-24 22:48:04 +0200

    Manage developement and production databases

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+
 export default {
-    mongoUrl: 'mongodb://localhost:27017/movies'
+    mongoUrl: process.env.NODE_ENV === 'development' 
+    ? 'mongodb://localhost:27017/movies' 
+    : `mongodb+srv://Rotarepmi:${process.env.MONGO}@movies-qdlml.mongodb.net/movies?retryWrites=true`
 }
